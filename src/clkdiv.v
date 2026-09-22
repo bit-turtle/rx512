4e47e2027a4839947feaa64ee47f1738339d9ee4
Michael Spears <mts18142027@gmail.com>
2026-09-21 21:03:46 -0400

    Updated ClockDivider

--- src/clkdiv.v
+++ src/clkdiv.v
@@ -1,3 +1,8 @@
+// Clock Divider
+
+`ifndef CLKDIV
+`define CLKDIV
+
 module ClockDivider #(
 	parameter DIVISOR = 250,
 	parameter BIT_WIDTH = 8
@@ -6,12 +11,21 @@ module ClockDivider #(
 	output reg o_clock
 );
 
+	// Counter
 	reg [BIT_WIDTH-1:0] r_counter = 0;
 
 	// Count and toggle on 0
 	always @(posedge i_clock) begin
-		r_counter <= (r_counter < DIVISOR-1) ? r_counter + 1 : 0;
-		o_clock <= (r_counter == 0) ? ~o_clock : o_clock;
+		if (r_counter == DIVISOR-1) begin
+			r_counter = 0;
+			o_clock = ~o_clock;
+		end
+		else begin
+			r_counter = r_counter + 1;
+			o_clock = o_clock;
+		end
 	end
 
 endmodule
+
+`endif // CLKDIV
